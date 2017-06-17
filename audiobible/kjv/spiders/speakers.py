139c51eb98ca36d64417e40b5a10052dc4cf6c88
Alex Goretoy <alex@goretoy.com>
2017-06-17 18:20:50 -0500

    added speakers spider

---
+++ audiobible/kjv/spiders/speakers.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import os
+import json
+import string
+from ..items import get_item_and_loader
+
+from scrapy.utils.project import get_project_settings as Settings
+settings = Settings()
+
+# http://www.sermonaudio.com/sermonsspeaker.asp?localsection=A
+# http://www.sermonaudio.com/search.asp?speakerWithinSource=&subsetCat=speaker&subsetItem=Rev+David+Silversides&mediatype=&includekeywords=&exactverse=&keyword=GENESIS&keyworddesc=Genesis+1&currsection=&AudioOnly=false&BibleOnly=true&Chapter=1&Verse=0
+
+
+class SpeakersSpider(scrapy.Spider):
+    name = "speakers"
+    allowed_domains = ["www.sermonaudio.com"]
+
+    def __init__(self):
+        self.start_urls = ["http://www.sermonaudio.com/sermonsspeaker.asp?localsection=%s" % l for l in list(string.ascii_uppercase)]
+
+    def parse(self, response):
+        if response.status in (200, ):
+            item, loader = get_item_and_loader('Speakers', keys=[
+                'name',
+                'path'
+            ])
+            l = loader(response=response)
+            names = l.get_xpath('//a[contains(@class, "navleftblack2b")][contains(@href, "speaker/")]/text()')
+            paths = l.get_xpath('//a[contains(@class, "navleftblack2b")][contains(@href, "speaker/")]/@href')
+            for n in range(len(names)):
+                itm = item()
+                itm['name'] = names[n].strip()
+                itm['path'] = paths[n].strip()
+
+                li = loader(item=itm)
+                yield li.load_item()
