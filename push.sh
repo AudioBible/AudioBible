5935a0050369201acdcc6182180b5c605d018dff
Alex Goretoy <alex@goretoy.com>
2017-06-29 23:41:02 -0500

    update

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git push && git push --tags
+git push ysfe && git push ysfe --tags
 git push up && git push up --tags
 git push bb && git push bb --tags
 git push sf && git push sf --tags
