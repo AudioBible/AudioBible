63d8709e05b323fd05370506504e0535dfc1aaea
Alex Goretoy <alex@goretoy.com>
2017-07-06 19:31:03 -0500

    update push

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cp *.md ~/KJV/ && git add *.md
+cp *.md ~/KJV/
 cp images/*.jpg ~/KJV/images/
 cp images/*.png ~/KJV/images/
 cp images/*.gif ~/KJV/images/
 d=`pwd`
-cd ~/KJV && git add images/* && git commit -am 'update' && git push; cd "$d"
+cd ~/KJV && git add images/* && git add *.md && git commit -am 'update' && git push; cd "$d"
 
 git push && git push --tags
 git push ysfe && git push ysfe --tags
