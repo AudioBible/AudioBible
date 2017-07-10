c159aef8a2de1a9f8a23629102352cc23bfc2fe7
Alex Goretoy <alex@goretoy.com>
2017-07-10 13:17:02 -0500

    force push

--- push.sh
+++ push.sh
@@ -8,11 +8,11 @@ if [ -d ~/KJV ]; then
     cp images/*.png ~/KJV/images/
     cp images/*.gif ~/KJV/images/
     d=`pwd`
-    cd ~/KJV && echo "$version" > version && git add version images/* *.md *.json && git commit -am 'update' && git push; cd "$d"
+    cd ~/KJV && echo "$version" > version && git add version images/* *.md *.json && git commit -am 'update' && git push -f; cd "$d"
 fi
 
 git push && git push --tags
-git push ysfe && git push ysfe --tags
-git push up && git push up --tags
-git push bb && git push bb --tags
-git push sf && git push sf --tags
+git push ysfe -f && git push ysfe --tags -f
+git push up -f && git push up --tags -f
+git push bb -f && git push bb --tags -f
+git push sf -f && git push sf --tags -f
