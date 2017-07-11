3d75445ee53309da0713f478e07bc6c969893e3a
Alex Goretoy <alex@goretoy.com>
2017-07-10 21:04:27 -0500

    update

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ if [ -d ~/KJV ]; then
 fi
 
 
-git fetch && git rebase origin/master master && git git push && git push --tags
+git fetch && git rebase origin/master master && git push && git push --tags
 git push ysfe -f && git push ysfe --tags -f
 git push up -f && git push up --tags -f
 git push bb -f && git push bb --tags -f
