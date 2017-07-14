df41a9afadf493646e95024d3fb780521c8daed4
Alex Goretoy <alex@goretoy.com>
2017-07-14 00:15:24 -0500

    update

--- push.sh
+++ push.sh
@@ -31,8 +31,8 @@ fi
 
 
 git fetch && git rebase origin/master master && git push && git push --tags
+git push abl -f && git push --tags -f
 git push ysfe -f && git push ysfe --tags -f
-git push abl -f && git push abl --tags -f
 git push up -f && git push up --tags -f
 git push ab -f && git push up --tags -f
 git push bb -f && git push bb --tags -f
