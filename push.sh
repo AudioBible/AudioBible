c7e1cb2b97838f6dee7bff5b43356cfabf99ca42
Alex Goretoy <alex@goretoy.com>
2017-09-07 20:38:51 +0700

    BIG BANG IS THEORY! FLAT EARTH IS TRUTH!

--- push.sh
+++ push.sh
@@ -4,6 +4,8 @@ version=`audiobible/__init__.py version`
 message="BIG BANG IS THEORY! FLAT EARTH IS TRUTH!"
 skip_images="`echo ${1:-"false"} | tr [a-z] [A-Z]`";
 
+trap sigint_handler SIGINT
+
 if [[ $skip_images == *"F"* ]]; then
     ./optimize_images.sh;
 else
