44accbe26ef25a7c176c98c1ed5909c90b6d7113
Alex Goretoy <alex@goretoy.com>
2017-09-10 00:21:48 +0700

    BIG BANG IS THEORY! FLAT EARTH IS TRUTH! 9/11 WAS AN INSIDE JOB!

--- push.sh
+++ push.sh
@@ -25,8 +25,9 @@ fi
 git add original_images/ images/ books/;
 
 function WRITE_IMAGES_FILE() {
+    local command="ls -1 images/|grep -v youtube-channel|grep -v youtube-search|xargs";
     local proceed="no"
-    for i in `ls -1 images/|grep -v youtube-channel|grep -v youtube-search|xargs`; do
+    for i in `$command`; do
         if [ "`grep -i "$i" IMAGES.md`" == "" ]; then
             local proceed="yes"
         fi
@@ -44,7 +45,7 @@ function WRITE_IMAGES_FILE() {
         echo "======" >> IMAGES.md;
         echo "" >> IMAGES.md;
 
-        for i in `ls -1 images/|grep -v youtube-channel|grep -v youtube-search|xargs`; do
+        for i in `$command`; do
             echo "- [$i](images/$i)" >> IMAGES.md;
             echo "" >> IMAGES.md;
             echo "![$i](images/$i)" >> IMAGES.md;
